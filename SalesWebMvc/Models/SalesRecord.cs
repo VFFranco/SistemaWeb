1523ebb388a1490ab7d7c65428bd8371f9a4127b
Vinicius Franco <viniffarias@hotmail.com>
2022-11-09 21:52:15 -0300

    Seeding service

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -8,19 +8,19 @@ namespace SalesWebMvc.Models
         public int Id{ get; set; }
         public DateTime Date { get; set; }
         public double Amount { get; set; }
-        public SalesStatus Status { get; set; }
+        public SaleStatus SaleStatus { get; set; }
         public Seller Seller { get; set; }
 
         public SalesRecord()
         {
         }
 
-        public SalesRecord(int id, DateTime date, double amount, SalesStatus status, Seller seller)
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus saletatus, Seller seller)
         {
             Id = id;
             Date = date;
             Amount = amount;
-            Status = status;
+            SaleStatus = saletatus;
             Seller = seller;
         }
     }
